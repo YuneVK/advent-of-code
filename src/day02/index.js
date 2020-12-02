3120792d05096e1fa4211d35c459a746aca2d2f5
Sonia Ruiz <yune.vk@gmail.com>
2020-12-02 21:43:09 +0100

    ✨ Day 2 - Part 2

--- src/day02/index.js
+++ src/day02/index.js
@@ -1,29 +1,30 @@
-const getInput = require('../utils/getInput')
+// const getInput = require('../utils/getInput')
+// const input = getInput('02')
 
-const input = getInput('02')
-
-const getTotalValidPasswords = lines => {
+const getTotalValidPasswordsPart1 = lines => {
   return lines.filter(line => {
     const [range, letter, password] = line.replace(':', '').split(' ')
     const [min, max] = range.split('-')
     const totalInPassword = password.split('').filter(chart => chart === letter).length
 
     return totalInPassword >= min && totalInPassword <= max
-
-    // with regex (WIP)
-    // const regex = new RegExp(`${letter}{${min},${max}}`)
-    // return regex.test(password)
   }).length
 }
 
-const data = [
-  '1-3 a: abcde',
-  '1-3 b: cdefg',
-  '2-9 c: ccccccccc'
-]
+const getTotalValidPasswordsPart2 = lines => {
+  return lines.filter(line => {
+    const [range, letter, password] = line.replace(':', '').split(' ')
+    const [position1, position2] = range.split('-')
 
-console.time('Part 1')
-console.log(getTotalValidPasswords(input))
-console.timeEnd('Part 1')
+    return (password[position1 - 1] === letter) !== (password[position2 - 1] === letter)
+  }).length
+}
 
-module.exports = { getTotalValidPasswords }
+// console.time('Part 1')
+// console.log(getTotalValidPasswordsPart1(input))
+// console.timeEnd('Part 1')
+// console.time('Part 2')
+// console.log(getTotalValidPasswordsPart2(data))
+// console.timeEnd('Part 2')
+
+module.exports = { getTotalValidPasswordsPart1, getTotalValidPasswordsPart2 }
--- src/day02/index.test.js
+++ src/day02/index.test.js
@@ -1,28 +1,32 @@
-const { getTotalValidPasswords } = require('./index')
+const { getTotalValidPasswordsPart1, getTotalValidPasswordsPart2 } = require('./index')
 const getInput = require('../utils/getInput')
 
 const input = getInput('02')
 
+const exampleData = [
+  '1-3 a: abcde',
+  '1-3 b: cdefg',
+  '2-9 c: ccccccccc'
+]
+
 describe('Day 2: Report Repair - Part 1', () => {
   describe('Part 1', () => {
     test('should work with an example', () => {
-      const data = [
-        '1-3 a: abcde',
-        '1-3 b: cdefg',
-        '2-9 c: ccccccccc'
-      ]
-
-      expect(getTotalValidPasswords(data)).toBe(2)
+      expect(getTotalValidPasswordsPart1(exampleData)).toBe(2)
     })
 
     test('should work with input', () => {
-      expect(getTotalValidPasswords(input)).toBe(600)
+      expect(getTotalValidPasswordsPart1(input)).toBe(600)
     })
   })
 
   describe('Part 2', () => {
+    test('should work with an example', () => {
+      expect(getTotalValidPasswordsPart2(exampleData)).toBe(1)
+    })
+
     test('should work with input', () => {
-      expect(true).toBe(true)
+      expect(getTotalValidPasswordsPart2(input)).toBe(245)
     })
   })
 })
